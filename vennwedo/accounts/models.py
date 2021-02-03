20a5ffe97ad6e79594c2ee23bc6507dd6e352eb4
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-02-03 15:59:15 +0530

    Add custom user model

--- vennwedo/accounts/models.py
+++ vennwedo/accounts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, username, email, name, password=None):
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            name="True",
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+
+    objects = UserManager()
+
+    username = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'name']
+
+    def __str__(self):
+        return self.username
