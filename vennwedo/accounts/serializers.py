8fba89b87054e285a3108111e7324d600346571c
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-02-03 23:42:34 +0530

    Update the serializer for accounts

--- vennwedo/accounts/serializers.py
+++ vennwedo/accounts/serializers.py
@@ -15,8 +15,8 @@ class CustomRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
 
     def create(self, validated_data):
-        user = User.objects.create_user(name=validated_data['name'], username=validated_data['username'],
-                                        email=validated_data['email'],
+        user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'],
+                                        name=validated_data['name'],
                                         password=make_password(
                                             validated_data['password']),
                                         )
