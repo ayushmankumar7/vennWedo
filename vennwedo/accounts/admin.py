bd190e03d975f9b313e66f775298aab46f7da1aa
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-02-04 11:57:40 +0530

    Update the admin.py to customize admin pannel

--- vennwedo/accounts/admin.py
+++ vennwedo/accounts/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from accounts.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(CustomUser)
+
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    list_display = ('username','name', 'email',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'name', 'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
+
+
+# admin.site.register(CustomUser, UserAdminConfig)
