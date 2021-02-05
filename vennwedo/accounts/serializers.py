0654268930a8769456e284f66223cc0f1634ddd0
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-02-05 23:18:32 +0530

    Delete unused imports and delete the create method

--- vennwedo/accounts/serializers.py
+++ vennwedo/accounts/serializers.py
@@ -1,8 +1,5 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
-
+from rest_framework import serializers
 
 User = get_user_model()
 
@@ -14,15 +11,6 @@ class CustomRegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
 
-    def create(self, validated_data):
-        user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'],
-                                        name=validated_data['name'],
-                                        password=make_password(
-                                            validated_data['password']),
-                                        )
-        return user
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'name', 'password')
-        read_only_fields = ('email',)
+        fields = ('id', 'username', 'email', 'name', 'password')
