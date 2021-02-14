bfc44cb3f9780ec4ac7e62665744cfb99093315b
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-02-14 23:19:59 +0530

    Add Backend app with video meta data model

---
+++ vennwedo/backend/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+User = get_user_model()
+
+class Video(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_bucket_id = models.CharField(max_length=50, null=True)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, blank=True)
+    thumbnail = models.ImageField(null=True)
+    publishing_date = models.DateTimeField(default=timezone.now, blank=True)
+
+    def __str__(self):
+        return self.title
