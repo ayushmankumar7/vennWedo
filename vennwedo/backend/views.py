bcec3194db0b9648aee472964d8b345eecd2ca6f
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-03-22 21:00:53 +0530

    Add views for video CRUD

--- vennwedo/backend/views.py
+++ vennwedo/backend/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from backend.serializer import (
+    VideoCreateSerializer,
+    VideoListSerializer,
+    VideoUpdateSerializer,
+    VideoDeleteSerializer
+)
+from backend.models import Video
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class VideoCreateApi(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Video.objects.all()
+    serializer_class = VideoCreateSerializer
+
+
+class VideoListApi(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Video.objects.all()
+    serializer_class = VideoListSerializer
+
+
+class VideoUpdateApi(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Video.objects.all()
+    serializer_class = VideoListSerializer
+
+
+class VideoDeleteApi(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Video.objects.all()
+    serializer_class = VideoDeleteSerializer
